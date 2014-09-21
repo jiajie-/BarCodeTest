c5d126b4516493c30d79d0c0e931325a05bd80dd
jiajie <654350754@qq.com>
2014-09-21 23:30:14 +0800

    First commit

---
+++ src/com/ericssonlabs/bean/User.java
@@ -0,0 +1,45 @@
+package com.ericssonlabs.bean;
+
+public class User {
+
+	private int userid;
+
+	private String userName;
+
+	private String account;
+
+	private String password;
+
+	public int getUserid() {
+		return userid;
+	}
+
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getAccount() {
+		return account;
+	}
+
+	public void setAccount(String account) {
+		this.account = account;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
